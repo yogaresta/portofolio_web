f79e6520af43805825aef5d7a1214c288d1d5999
yogaresta <yogaresta96@gmail.com>
2023-06-24 16:13:06 +0700

    commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1> Main</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Topbar.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function Topbar() {
+  return (
+   <div className="Headertop">
+      <div id='topbar'>
+       <h1>topbar</h1>
+      </div>
+   </div>
+  );
+}
+export default Topbar;
